068cdb923bdf2ba58cf3c2413448ea1c333266e1
Trevor <sales@merrows.co.uk>
2017-04-27 09:22:23 +0100

    truffle

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,10 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var People = artifacts.require("./People.sol");
 var Review = artifacts.require("./Review.sol");
-var Zipfs = artifacts.require("./Zipfs.sol");
- module.exports = function(deployer) {
+module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, People);
   deployer.deploy(People);
   deployer.link(ConvertLib, Review);
   deployer.deploy(Review);
-  deployer.link(ConvertLib, Zipfs);
-  deployer.deploy(Zipfs);
-
 };
